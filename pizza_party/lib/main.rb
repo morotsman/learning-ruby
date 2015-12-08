7b14990af4aff43f7d0b7752084d494359301e16
niklas.leopold <leopold.niklas@gmail.com>
2015-12-08 19:56:27 +0100

    added more exercises

--- pizza_party/lib/main.rb
+++ pizza_party/lib/main.rb
@@ -10,17 +10,26 @@ end
 number_of_people = ""
 until is_integer(number_of_people)
   puts "How many people?"
-  number_of_people = gets.strip
+  number_of_people = gets.strip.to_i
 end
 
 number_of_pizza = ""
 until is_integer(number_of_pizza)
   puts "How many pizzas do you have?"
-  number_of_pizza = gets.strip
+  number_of_pizza = gets.strip.to_i
 end
 
 number_of_slices = ""
 until is_integer(number_of_slices)
   puts "How many slices per pizza?"
-  number_of_slices = gets.strip
+  number_of_slices = gets.strip.to_i
 end
+
+slices = number_of_pizza * number_of_slices
+
+slices_per_person = slices / number_of_people
+leftover_slices = slices % number_of_people
+
+puts "Each person gets #{slices_per_person} pieces of pizza."
+
+puts "There are #{leftover_slices} leftover pieces."
